1ef03339a38e5a54935bb4f65f818bd22fc245aa
sequester0 <i.keremgumus@hotmail.com>
2022-08-11 16:21:31 +0300

    dependency injection for product

--- OrderManagement.API/Program.cs
+++ OrderManagement.API/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IBasketBusinessEngine, BasketBusinessEngine>();
+builder.Services.AddScoped<IProductBusinessEngine, ProductBusinessEngine>();
 
 var app = builder.Build();
 
