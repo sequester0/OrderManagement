7b25ddcc719ff41c5e858aff75064af43f15f127
sequester0 <i.keremgumus@hotmail.com>
2022-08-11 16:25:31 +0300

    added product

---
+++ OrderManagement.Common/Contracts/IProductBusinessEngine.cs
@@ -0,0 +1,19 @@
+﻿using OrderManagement.Common.DTO.Product;
+using OrderManagement.Common.Result;
+using OrderManagement.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Common.Contracts
+{
+    public interface IProductBusinessEngine
+    {
+        List<Product> Get();
+        Result<ProductDto> GetProductById(int productid);
+        Result<Product> Add(ProductCreateDto productCreateDto);
+        Result<Product> Remove(int productid);
+    }
+}
---
+++ OrderManagement.Data/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Data.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
