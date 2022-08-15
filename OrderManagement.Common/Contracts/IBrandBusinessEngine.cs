93e7d8cebf387a15e8b5568e696c41bf017c6a61
sequester0 <i.keremgumus@hotmail.com>
2022-08-15 21:17:05 +0300

    dependency injection for brand

---
+++ OrderManagement.Common/Contracts/IBrandBusinessEngine.cs
@@ -0,0 +1,20 @@
+﻿using OrderManagement.Common.DTO.Brand;
+using OrderManagement.Common.Result;
+using OrderManagement.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Common.Contracts
+{
+    public interface IBrandBusinessEngine
+    {
+        Result<IEnumerable<BrandDto>> GetBrands();
+        Result<IEnumerable<BrandDto>> GetBrandById(int brandid);
+        Result<Brand> Add(BrandCreateDto brandCreateDto);
+        Result<Brand> Remove(int brandid);
+        Result<dynamic> UpdateBrandName(int brandid, BrandCreateDto brandCreateDto);
+    }
+}
